a356a3e90eb92d009ff0bc2235ac873c2dc214ba
gue1971 <gue1971@gmail.com>
2025-09-29 20:18:18 +0900

    整理: DS_Store削除 & horses.jsonなど更新

--- sw.js
+++ sw.js
@@ -48,32 +48,3 @@ self.addEventListener('fetch', e => {
 });
 
 self.addEventListener('message', e => { if (e.data?.type === 'SKIP_WAITING') self.skipWaiting(); });
-
-// const CACHE = 'mystable-v1';
-// const ASSETS = [
-//   './',
-//   './index.html',
-//   './data/horses.json',
-//   './icons/icon-192.png',
-//   './icons/icon-512.png',
-//   './icons/apple-touch-icon.png',
-//   './icons/favicon-32x32.png',
-//   // 画像など増やすならここに相対で追加
-// ];
-// self.addEventListener('install', e => {
-//   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-//   self.skipWaiting();
-// });
-// self.addEventListener('activate', e => {
-//   e.waitUntil(
-//     caches.keys().then(keys =>
-//       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-//     )
-//   );
-//   self.clients.claim();
-// });
-// self.addEventListener('fetch', e => {
-//   e.respondWith(
-//     caches.match(e.request).then(r => r || fetch(e.request))
-//   );
-// });
